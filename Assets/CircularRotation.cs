186e0123c2fbf5fb5ec7ac4655b2765700f94a8d
VeerendraDumadu <hema.veerendra@dumadu.com>
2018-11-09 15:47:54 +0530

    Myfirstproject

---
+++ Assets/CircularRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularRotation : MonoBehaviour {
+	public Transform center;
+	public float radius = 25;
+	public float angle = 0;
+	public float period = 10f;
+
+	// Update is called once per frame
+	void Update () {
+		angle += period*Time.deltaTime; 
+		float x = Mathf.Cos(angle)*radius + center.transform.position.x; //x=cos(angle)*R+a;
+		float z = Mathf.Sin(angle)*radius + center.transform.position.z; //y=sin(angle)*R+b;
+		this.gameObject.transform.position = new Vector2 (x,z);
+	}
+
+}
---
+++ Assets/Playermoment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermoment : MonoBehaviour {
+	//private PlayerController playerController;
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+	}
+	void Update () {
+		
+		transform.Translate (Vector3.forward * 10 * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			transform.Rotate (Vector3.up, 90);
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			transform.Rotate (Vector3.up, 90);
+		}
+	}
+}
